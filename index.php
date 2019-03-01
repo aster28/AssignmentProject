e4797536db94d37162888bd5e7e61aeebd569644
Sonia Singla <ssoniyaster@gmail.com>
2019-03-01 16:24:06 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ if(isset($_POST['submit'])){
     $rand= document.write(generateOTP());
     $msg = "The OTP is '.$rand.';
     $to = $_POST['email'];
-    $from ="ssoniyaster@gmail.com";
-    mail($to, 'My Subject', $message);
-    
+    $from =$_POST["ssoniyaster@gmail.com"];
+    mail($to, $from, 'My Subject', $message);
+    header('Location: thank_you.php')
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
