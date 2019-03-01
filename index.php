81bf6bea9ce2c5cdaf18b53549cff89fe91eafb8
Sonia Singla <ssoniyaster@gmail.com>
2019-03-01 16:26:31 +0000

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if(isset($_POST['submit'])){
     $to = $_POST['email'];
     $from =$_POST["ssoniyaster@gmail.com"];
     mail($to, $from, 'My Subject', $message);
-    header('Location: thank_you.php')
+    header('Location: thankyou.php')
     // You can also use header('Location: thank_you.php'); to redirect to another page.
     // You cannot use header and echo together. It's one or the other.
     }
