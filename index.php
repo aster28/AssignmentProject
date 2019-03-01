207750b2eb94669dac78480ae84e727a24cc1d94
Sonia Singla <ssoniyaster@gmail.com>
2019-03-01 16:33:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
-if(isset($_GET['submit'])){
+if(isset($_POST['submit'])){
     $rand= document.write(generateOTP());
     $msg = "The OTP is '.$rand.';
-    $to = $_GET['email'];
-    $from =$_GET["ssoniyaster@gmail.com"];
+    $to = $_POST['email'];
+    $from =$_POST["ssoniyaster@gmail.com"];
     mail($to, $from, 'My Subject', $message);
     header('Location: thankyou.php')
     // You can also use header('Location: thank_you.php'); to redirect to another page.
