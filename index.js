a533ce2601a4b93254af25e85cf19eb666fa3618
Sonia Singla <ssoniyaster@gmail.com>
2019-03-01 15:53:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,3 +18,23 @@ $(".signup").click(function(){
 $(".btn").click(function(){
   $(".input").val("");
 });
+function generateOTP() { 
+          
+    // Declare a digits variable  
+    // which stores all digits 
+    var digits = '0123456789'; 
+    let OTP = ''; 
+    for (let i = 0; i < 4; i++ ) { 
+        OTP += digits[Math.floor(Math.random() * 10)]; 
+    } 
+    return OTP; 
+} 
+function sendMail()
+{
+   var mailBody=document.getElementById('mailBody').innerHTML;
+   window.location="mailto:yourmail@domain.com?subject=document.write (generateOTP())&body="+mailBody;
+}
+    
+
+
+  
